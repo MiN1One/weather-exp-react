cd7646bf1f55165ec17eeb3341321c8e3cc0673a
Nodirbek U <66454953+MiN1One@users.noreply.github.com>
2020-09-05 22:55:20 +0500

    Fixed recentsearch

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
 import classes from './App.module.scss';
 import Toolbar from './Toolbar/Toolbar';
 import Data from './Data/Data';
 import Footer from '../components/Footer/Footer';
-import * as actions from '../store/actions/index';
 
 function App() {
 
@@ -21,4 +18,4 @@ function App() {
   );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(App));
+export default App;
